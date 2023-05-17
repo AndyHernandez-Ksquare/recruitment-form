e76226bba7ef14fe96bf2d08ea487d1c63340401
AndyHernandez-Ksquare <Andy.Hernandez@itksquare.edu.mx>
2023-05-17 10:56:47 -0400

    Created form hook to take out all logic from RecruitmentForm component

--- src/components/RecruitmentForm/RecruitmentForm.tsx
+++ src/components/RecruitmentForm/RecruitmentForm.tsx
@@ -4,94 +4,18 @@ import { PersonalInformation } from "../PersonalInformation";
 import { AddressInformation } from "../AddressInformation";
 import { AddressExtraInfo } from "../AddressExtraInfo";
 import { useFormik } from "formik";
-import { PersonalInformationValues } from "../../interfaces/PersonalInformation";
-import usePersonalInformationValues from "../../hooks/valluesHooks/usePersonalInformationValues";
-import useAddressInformationValues from "../../hooks/valluesHooks/useAddressInformationValues";
-import { AddressInformationValues } from "../../interfaces/AddressInformation";
-import { AddressExtraInfoValues } from "../../interfaces/AddressExtraInfo";
-import useAddressExtraInfoValues from "../../hooks/valluesHooks/useAddressExtraInfoValues";
-import useExtraPersonalInfoValues from "../../hooks/valluesHooks/useExtraPersonalInfoValues";
-import { ExtraPersonalInfoValues } from "../../interfaces/ExtraPersonalInfo";
 import { ExtraPersonalInfo } from "../ExtraPersonalInfo";
-import * as Yup from "yup";
-import { GobernamentalInfoValues } from "../../interfaces/GobernamentalInfo";
 import { GobernamentalInfo } from "../GobernamentalInfo";
-import { BankAccountInfoValues } from "../../interfaces/BankAccountInfo";
-import useBankAccountInfoValues from "../../hooks/valluesHooks/useBankAccountInfoValues";
 import { BankAccountInfo } from "../BankAccountInfo";
 import { Skills } from "../Skills";
-import useSkillsValues from "../../hooks/valluesHooks/useSkillsValues";
-import { SkillsValues } from "../../interfaces/Skills";
-
-// This type is important so that I can pass the same props to all components
-type FormValues = PersonalInformationValues &
-  AddressInformationValues &
-  AddressExtraInfoValues &
-  GobernamentalInfoValues &
-  ExtraPersonalInfoValues &
-  BankAccountInfoValues &
-  SkillsValues;
+import { FormValues } from "../../interfaces/Form";
+import useFormValuesAndValidation from "../../hooks/valuesHooks/useFormValuesAndValidation";
 
 export const RecruitmentForm = () => {
-  const { valuesPersonalInformation } = usePersonalInformationValues();
-
-  const { valuesAddresslInformation } = useAddressInformationValues();
-
-  const { valuesAddressExtraInfo } = useAddressExtraInfoValues();
-
-  const { valuesExtraPersonalInfo } = useExtraPersonalInfoValues();
-
-  const { valuesBankAccountInfo } = useBankAccountInfoValues();
-
-  const { valuesSkills } = useSkillsValues();
-
-  const validationSchema = Yup.object({
-    // Personal Info
-    name: Yup.string().required("Name is required"),
-    paternal_last_name: Yup.string().required("Paternal last name is required"),
-    gender: Yup.string().required("Gender is required"),
-    city_birth: Yup.string().required("City of birth is required"),
-    state_birth: Yup.string().required("State of birth is required"),
-    country: Yup.string().required("Country is required"),
-
-    // Address Info
-    street: Yup.string().required("Street is required"),
-    streetA: Yup.string().required("StreetA is required"),
-    colony: Yup.string().required("Colony is required"),
-
-    // Address extra info
-    identificationCurp: Yup.string().matches(
-      /^[A-Z0-9]{18}$/,
-      "This is not a valid CURP document"
-    ),
-    identificationPassport: Yup.string().matches(
-      /^[A-Z0-9]{6,9}$/,
-      "This is not a valid passport document"
-    ),
-
-    // Extra personal info
-    email: Yup.string().email("This is not an email"),
-    altEmail: Yup.string().email("This is not an email"),
-    phone: Yup.string().matches(/^\d{1,2} \d{10}$/, "Invalid phone number"),
-
-    // Bank account info
-    accountNumber: Yup.string().matches(
-      /^[A-Z0-9]+$/,
-      "This is not a valid account number"
-    ),
-
-    CLABE: Yup.string().matches(/^\d{18}$/, "This is not a valid CLABE"),
-  });
+  const { valuesForm, validationSchema } = useFormValuesAndValidation();
 
   const formik = useFormik<FormValues>({
-    initialValues: {
-      ...valuesPersonalInformation,
-      ...valuesAddresslInformation,
-      ...valuesAddressExtraInfo,
-      ...valuesExtraPersonalInfo,
-      ...valuesBankAccountInfo,
-      ...valuesSkills,
-    },
+    initialValues: valuesForm,
     // validate: combinedValidation,
     validationSchema: validationSchema,
     onSubmit: (value) => {
---
+++ src/hooks/valuesHooks/useFormValuesAndValidation.ts
@@ -0,0 +1,71 @@
+import { FormValues } from "../../interfaces/Form";
+import useAddressExtraInfoValues from "./useAddressExtraInfoValues";
+import useAddressInformationValues from "./useAddressInformationValues";
+import useBankAccountInfoValues from "./useBankAccountInfoValues";
+import useExtraPersonalInfoValues from "./useExtraPersonalInfoValues";
+import usePersonalInformationValues from "./usePersonalInformationValues";
+import useSkillsValues from "./useSkillsValues";
+import * as Yup from "yup";
+
+const useFormValuesAndValidation = () => {
+  const { valuesPersonalInformation } = usePersonalInformationValues();
+
+  const { valuesAddresslInformation } = useAddressInformationValues();
+
+  const { valuesAddressExtraInfo } = useAddressExtraInfoValues();
+
+  const { valuesExtraPersonalInfo } = useExtraPersonalInfoValues();
+
+  const { valuesBankAccountInfo } = useBankAccountInfoValues();
+
+  const { valuesSkills } = useSkillsValues();
+  let valuesForm: FormValues = {
+    ...valuesPersonalInformation,
+    ...valuesAddresslInformation,
+    ...valuesAddressExtraInfo,
+    ...valuesExtraPersonalInfo,
+    ...valuesBankAccountInfo,
+    ...valuesSkills,
+  };
+
+  const validationSchema = Yup.object({
+    // Personal Info
+    name: Yup.string().required("Name is required"),
+    paternal_last_name: Yup.string().required("Paternal last name is required"),
+    gender: Yup.string().required("Gender is required"),
+    city_birth: Yup.string().required("City of birth is required"),
+    state_birth: Yup.string().required("State of birth is required"),
+    country: Yup.string().required("Country is required"),
+
+    // Address Info
+    street: Yup.string().required("Street is required"),
+    streetA: Yup.string().required("StreetA is required"),
+    colony: Yup.string().required("Colony is required"),
+
+    // Address extra info
+    identificationCurp: Yup.string().matches(
+      /^[A-Z0-9]{18}$/,
+      "This is not a valid CURP document"
+    ),
+    identificationPassport: Yup.string().matches(
+      /^[A-Z0-9]{6,9}$/,
+      "This is not a valid passport document"
+    ),
+
+    // Extra personal info
+    email: Yup.string().email("This is not an email"),
+    altEmail: Yup.string().email("This is not an email"),
+    phone: Yup.string().matches(/^\d{1,2} \d{10}$/, "Invalid phone number"),
+
+    // Bank account info
+    accountNumber: Yup.string().matches(
+      /^[A-Z0-9]+$/,
+      "This is not a valid account number"
+    ),
+
+    CLABE: Yup.string().matches(/^\d{18}$/, "This is not a valid CLABE"),
+  });
+
+  return { valuesForm, validationSchema };
+};
+export default useFormValuesAndValidation;
